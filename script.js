89eb148ea4e6188beb47ff16800fd4966127bb99
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-14 22:56:36 +0530

    Define createGrid function to create the grid
    Inner and outer divs are created and event
    listeners are attached to inner divs. Then, outer
    divs are appended to grid container element.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let blackIntensity = 0;
 const colorModeButton = document.querySelector('.random-color-mode');
 const blackModeButton = document.querySelector('.black-color-mode');
 const resizeButton = document.querySelector('.resize-btn');
+const buttonContainer = document.querySelector('.btn-container');
+const gridContainer = document.querySelector('.container');
 
 colorModeButton.addEventListener('click', (e) => {
   fillMode = 'color';
@@ -20,6 +22,32 @@ resizeButton.addEventListener('click', (e) => {
   createGrid();
 });
 
+function createGrid() {
+  for (let i = 0; i < squareGridLength; i++) {
+    let outerDiv = document.createElement('div');
+    outerDiv.style.width = "960px";
+    outerDiv.style.height = `${960 / squareGridLength}px`;
+    for (let j = 0; j < squareGridLength; j++) {
+      let innerDiv = document.createElement('div');
+      innerDiv.style.height = `${960 / squareGridLength}px`;
+      innerDiv.style.width = `${960 / squareGridLength}px`;
+      innerDiv.addEventListener('mouseover', (e) => {
+        if (fillMode === 'black') {
+          blackIntensity += 1;
+          innerDiv.style.backgroundColor = `rgba(0,0,0,${blackIntensity * (0.1)}`;
+          if (blackIntensity === 10) {
+            blackIntensity %= 10;
+          }
+        }
+        else {
+          innerDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})}`;
+        }
+      });
+      outerDiv.appendChild(innerDiv);
+    }
+    gridContainer.appendChild(outerDiv);
+  }
+}
 
 createGrid();
 
